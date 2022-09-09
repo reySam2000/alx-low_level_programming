8702f346d4ad53835f4399b7d14e009774b459a0
reySam2000 <onuchesamuel09@gmail.com>
2022-09-08 18:30:28 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ *using the put function 
+ *This program prints "Programming is like building a multilingual puzzle"
+ */
+int main()
+{
+  puts("Programming is like building a multilingual puzzle");
+  return 0;
+}
