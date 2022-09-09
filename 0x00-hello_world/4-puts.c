48f4c6deec28989bcf5a5ee8521e477843138f8b
reySam2000 <onuchesamuel09@gmail.com>
2022-09-08 20:14:49 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main-prints"Programming is like building a multilingual puzzle 
+ * main-prints"Programming is like building a multilingual puzzle
  *
- * Return:0 if exited properly 
+ * Return:0 if exited properly
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
