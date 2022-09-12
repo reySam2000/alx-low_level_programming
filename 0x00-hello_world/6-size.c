63a6d8481e2d0fe26053e0378bb798241231c8ec
reySam2000 <onuchesamuel09@gmail.com>
2022-09-12 15:26:32 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 /**
- * main-print various type of sizes
- * Return: 0 if exited properly
+ * main - print the string in the put function
+ *
+ * Return: 0 if it's a success
  */
 int main(void)
 {
 	printf("size of a char: %d byte(s)\n", sizeof(char));
 	printf("size of an int: %d byte(s)\n", sizeof(int));
-	printf("size of a longint: %d byte(s)\n", sizeof(longint));
-	printf("size of a longlongint: %d byte(s)\n", sizeof(longlongint));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
-
