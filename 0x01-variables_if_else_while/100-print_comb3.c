c00dd4a3171b98f59240e0f1842b31fd10886876
reySam2000 <onuchesamuel09@gmail.com>
2022-09-11 17:01:21 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 /**
- * main-prints combo of two different digits
+ * print combo of two different digits
  * and only print the smallest combo of the two digits
  * Return: 0 if it's a success
  */
 int main(void)
 {
 	int n, m;
+
 	for (n = 48 ; n <= 56 ; n++)
 	{
 		for (m = 49 ; m <= 57 ; m++)
