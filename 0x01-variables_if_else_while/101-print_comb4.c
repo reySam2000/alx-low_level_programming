83946bb239ff650650daf8111a1d3a6cf0aea9f6
reySam2000 <onuchesamuel09@gmail.com>
2022-09-11 19:10:05 -0700

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible different combination of three digits
+ *
+ * Return: 0 if it's a success
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 48 ; x = <= 55 ; x++)
+	{
+		for (y = 49 ; y <= 56 ; y++)
+		{
+			for (z = 50 ; z <= 57 ; z++)
+			{
+				if (z > y && y > x)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x != 55 || y != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
