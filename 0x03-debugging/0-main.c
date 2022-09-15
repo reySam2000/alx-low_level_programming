bccde2ce81ba0b53982ab63942d79681b0564dbb
reySam2000 <onuchesamuel09@gmail.com>
2022-09-14 20:13:43 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -7,12 +8,11 @@
 
 int main(void)
 {
-	int i;
+	        int i;
 
-	i = 0;
-	positive_or_negative(i);
+		        i = 98;
+			        positive_or_negative(i);
 
-	return (0);
+				        return (0);
 }
-
 
