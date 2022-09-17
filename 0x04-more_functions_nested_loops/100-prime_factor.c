6ba8e6deee9cf4b1dbfd926c4c4ff6cb9fd3da42
reySam2000 <onuchesamuel09@gmail.com>
2022-09-16 19:42:23 -0700

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the largest prime factor of number
+ * main - prints the largest prime factor of number
  * followed by a new line
  *
  * Return: 0 if it's a success
@@ -19,11 +19,9 @@ int main(void)
 			number /= 2;
 			continue;
 		}
-
 		for (inc = 3 ; inc < number / 2 ; inc += 2)
 		{
 		  if (number % inc == 0)
-
 		    number /= inc;
 		}
 	}
