1b035e6ed2fbbb4572b548be1ce0c0a0eacc30f8
reySam2000 <onuchesamuel09@gmail.com>
2022-09-22 20:09:45 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalize all words in a string
+ * @s: string to modify
+ *
+ * Return: the result
+ */
+char *cap_string(char *s)
+{
+	int x, y;
+
+	char wat[13] = {' ', '\t', '\n', ',', ';', '.','!', '?', '"', '(', ')', '{', '}'};
+
+	for (x = 0 ; s[x] != '\0' ; x++)
+	{
+		if (x == 0 &&s[x] >= 'a' && s[x] <= 'z')
+			s[x] -= 32;
+
+		for (y = 0 ; y < 13 ; y++)
+		{
+			if (s[x] == wat[y])
+			{
+				if (s[x + 1] >= 'a' && s[x + 1] <= 'z')
+				{
+					s[x + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
+}
