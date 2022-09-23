7e4d7757639da880a7715c6cccab0655193182ca
reySam2000 <onuchesamuel09@gmail.com>
2022-09-22 18:07:25 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,14 +13,17 @@ char *_strncat(char *dest, char *src, int n)
 	int x, y;
 
 	x = 0;
+	
 	while (dest[x] != '\0')
 	  {
 		x++;
 	  }
+	
 	for (y = 0 ; y < n && src[y] != '\0' ; y++, x++)
 	  {
 		dest[x] = src[y];
 	  }
 	dest[x] = '\0';
+	
 	return (dest);
 }
