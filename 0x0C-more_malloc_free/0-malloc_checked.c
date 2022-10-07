a8808aeacf28205b0715578c5f3b96b54d518740
reySam2000 <onuchesamuel09@gmail.com>
2022-10-06 17:17:03 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,14 +7,15 @@
  *
  * Return: pointer to the allocated memory or normal process termination on error
  */
+
 void *malloc_checked(unsigned int b)
 {
-
-
 	void *m;
 
 	m = malloc(b);
+
 	if (m == NULL)
 		exit(98);
+
 	return (m);
 }
